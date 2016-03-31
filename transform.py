7e138c23c3761c7ef6e67c241db0a7f50b109393
Ben Wiley <therealbenwiley@gmail.com>
2016-03-31 04:13:32 -0400

    Python restructuring function included as default; JavaScript method left as fallback.

--- transform.py
+++ transform.py
@@ -7,12 +7,12 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '-n',
-    '--now',
+    '-l',
+    '--lazy',
     action='store_true',
-    help='If provided, tries to use PyQuery to ' +
-        'restructure document now. If PyQuery is not ' +
-        'available, falls back to JavaScript.',
+    help='If this flag is provided, we won\'t try using Python to ' +
+        'restructure HTML document and will always insert JavaScript ' +
+        'to be run on page load instead.',
     default=False
 )
 parser.add_argument(
@@ -132,21 +132,74 @@ if (options.length) {
     script_tag = '<script async class="restructure_script">\n' + onload_script + '</script>\n'
     return include_in_head(script_tag, html)
 
-def pyquery_restructure (html):
-    pass
+def soup_restructure (html):
+    soup = BeautifulSoup(html, 'lxml')
+
+    # purge all br elements
+    [br.extract() for br in soup('br')]
+
+    # get rid of center elements and keep contents
+    [center.unwrap() for center in soup('center')]
+
+    # add pico8_container class to container element
+    container = soup.find(id='canvas').parent
+    container_class = 'pico8_container'
+    existing_class = str(container.get('class') or '')
+    container['class'] = (
+        existing_class.replace(container_class, '') +
+        ' ' + container_class
+    ).strip()
+
+    # remove element-specific width from container element
+    container_style = cssutils.parseStyle(container.get('style'))
+    if container_style:
+        container_style.setProperty('width', None)
+        new_style = container_style.cssText.replace('\n', '')
+        if len(new_style):
+            container['style'] = new_style
+        else:
+            del container['style']
+
+    # move pico 8 options to new options container element
+    options = soup(class_='pico8_el')
+    if len(options):
+        options_container = soup.new_tag('div')
+        options_container['class'] = 'pico8_options'
+        options[0].insert_before(options_container)
+        for option in options:
+            options_container.append(option)
+
+    return soup.prettify()
 
 
 restructure = javascript_restructure
-if args['now']:
+if not args['lazy']:
+    soup_available = True
+    lxml_available = True
+    cssutils_available = True
     try:
-        from pyquery import PyQuery as pq
-        restructure = pyquery_restructure
-    except NameError:
-        print(
-            'WARNING: "pyquery" module unavailable; ' +
-            'using JavaScript (to be run in browser) as ' +
-            'restructuring fallback.'
-        )
+        from bs4 import BeautifulSoup
+    except:
+        soup_available = False
+    try:
+        import lxml
+    except:
+        lxml_available = False
+    try:
+        import cssutils
+    except:
+        cssutils_available = False
+
+    if not (soup_available and lxml_available and cssutils_available):
+        if not soup_available:
+            print('[WARNING]: "beautifulsoup4" module unavailable.')
+        if not lxml_available:
+            print('[WARNING]: "lxml" module unavailable.')
+        if not cssutils_available:
+            print('[WARNING]: "cssutils" module unavailable.')
+        print('Using JavaScript (to be run in browser) as restructuring fallback.')
+    else:
+        restructure = soup_restructure
 
 
 filename = args['filename']
